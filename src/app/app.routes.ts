a9dbb9846938d183f6922b42c33b7ccabda98ddb
redenbacher2003 <169102472+redenbacher2003@users.noreply.github.com>
2025-04-15 23:20:59 -0400

    updated routes, style dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,22 @@ import { Routes } from '@angular/router';
 import { SpeciesComponent } from './species/species.component';
 import { HomeComponent } from './home/home.component';
 import { BirdCompomentComponent } from './bird-compoment/bird-compoment.component';
+import { DashbboardComponent } from './dashbboard/dashbboard.component';
 export const routes: Routes = [
   {
     path: '',
+    component: DashbboardComponent // Set DashbboardComponent as the default route
+  },
+
+  { path: 'species', component: SpeciesComponent },
+  { path: 'bird/:id', component: BirdCompomentComponent },
+  {
+    path: 'home',
     component: HomeComponent,
     children: [
       { path: 'species', component: SpeciesComponent },
-      { path: 'bird/:id', component: BirdCompomentComponent }       
+      { path: 'bird/:id', component: BirdCompomentComponent },
+      { path: 'dashboard', component: DashbboardComponent }
     ]
   }
 ];
